40af1918ee5bddbb337e20bef6f1527f0140a10d
Nate Lust <natelust@gmail.com>
2014-02-12 23:17:28 -0500

    initial commit

---
+++ plugin_manager.py
@@ -0,0 +1,76 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import os
+import plugin_manager as me
+from zipfile import ZipFile
+from io import BytesIO
+import urllib.request as urlr
+import json
+
+
+from .plugman import Ui_PluginManager
+from .list_item import Ui_listitem
+
+#this is to get the directory of the containing module to get resources as other
+#programs will be actually running this module
+
+loc = me.__file__.rstrip('__init__.py')
+
+class myThread(QThread):
+    def __init__(self,url,parent):
+        QThread.__init__(self,parent=parent)
+        self.parent = parent
+        self.url = url
+    def run(self):
+        try:
+            web = urlr.urlopen(url)
+            by = BytesIO(self.url.read())
+            zp = ZipFile(by)
+            self.parent.zp = zp
+            for item in zp.namelist():
+                if 'manifest' in item:
+                    man = item
+            rd = zp.open(man).read()
+            ld = json.loads(rd.decode())
+            version = ld['version']
+            self.parent.latestversion.clear()
+            self.parent.setText(str(version))
+            self.parent.updatebutton.enabled(True)
+        except:
+            self.parent.latestversion.clear()
+            self.parent.latestversion.setFrameStyle(QFrame.StyledPanel|QFrame.Raised)
+            self.parent.latestversion.setText('Unavailable')
+
+
+class plugin_item(QWidget,Ui_listitem):
+    def __init__(self,plugin,main):
+        QWidget.__init__(self)
+        self.setupUi(self)
+        self.plugin = plugin
+        self.main = main
+        self.name.setText(plugin['manifest']['name'])
+        self.description.setText(plugin['manifest']['description'])
+        self.installedversion.setText(str(plugin['manifest']['version']))
+        move = QMovie(os.path.join(loc,'working2.gif'),QByteArray(),self)
+        #move.setScaledSize(QSize(72,47))
+        self.latestversion.setMovie(move)
+        move.start()
+        thread = myThread(plugin['manifest']['url'],parent=self)
+        thread.start()
+        self.connect(self.updatebutton,SIGNAL('clicked()'),self.install)
+    def install(self):
+        self.zp.extractall(path=self.main.main.user_loc)
+
+
+
+class plugin_manager(QDialog,Ui_PluginManager):
+    def __init__(self,main):
+        QDialog.__init__(self)
+        self.setupUi(self)
+        self.main = main
+        self.setPalette(main.palette())
+        for plug in main.plugins_global_dict:
+            tmp = plugin_item(main.plugins_global_dict[plug],self)
+            self.local_group_lay.addWidget(tmp)
+        QObject.connect(self.accept,SIGNAL('clicked()'),self.close)
+        self.exec_()
---
+++ plugman.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'plugman.ui'
+#
+# Created: Mon Feb 10 01:55:00 2014
+#      by: PyQt4 UI code generator 4.10.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_PluginManager(object):
+    def setupUi(self, PluginManager):
+        PluginManager.setObjectName(_fromUtf8("PluginManager"))
+        PluginManager.setWindowModality(QtCore.Qt.ApplicationModal)
+        PluginManager.resize(646, 584)
+        self.verticalLayout_4 = QtGui.QVBoxLayout(PluginManager)
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.tabWidget = QtGui.QTabWidget(PluginManager)
+        self.tabWidget.setObjectName(_fromUtf8("tabWidget"))
+        self.tab = QtGui.QWidget()
+        self.tab.setObjectName(_fromUtf8("tab"))
+        self.verticalLayout_5 = QtGui.QVBoxLayout(self.tab)
+        self.verticalLayout_5.setObjectName(_fromUtf8("verticalLayout_5"))
+        self.scrollArea_2 = QtGui.QScrollArea(self.tab)
+        self.scrollArea_2.setWidgetResizable(True)
+        self.scrollArea_2.setObjectName(_fromUtf8("scrollArea_2"))
+        self.scrollAreaWidgetContents_2 = QtGui.QWidget()
+        self.scrollAreaWidgetContents_2.setGeometry(QtCore.QRect(0, 0, 562, 455))
+        self.scrollAreaWidgetContents_2.setObjectName(_fromUtf8("scrollAreaWidgetContents_2"))
+        self.verticalLayout_9 = QtGui.QVBoxLayout(self.scrollAreaWidgetContents_2)
+        self.verticalLayout_9.setObjectName(_fromUtf8("verticalLayout_9"))
+        self.local_group = QtGui.QWidget(self.scrollAreaWidgetContents_2)
+        self.local_group.setObjectName(_fromUtf8("local_group"))
+        self.verticalLayout_13 = QtGui.QVBoxLayout(self.local_group)
+        self.verticalLayout_13.setMargin(0)
+        self.verticalLayout_13.setObjectName(_fromUtf8("verticalLayout_13"))
+        self.local_group_lay = QtGui.QVBoxLayout()
+        self.local_group_lay.setObjectName(_fromUtf8("local_group_lay"))
+        self.verticalLayout_13.addLayout(self.local_group_lay)
+        self.verticalLayout_9.addWidget(self.local_group)
+        self.scrollArea_2.setWidget(self.scrollAreaWidgetContents_2)
+        self.verticalLayout_5.addWidget(self.scrollArea_2)
+        self.tabWidget.addTab(self.tab, _fromUtf8(""))
+        self.tab_2 = QtGui.QWidget()
+        self.tab_2.setObjectName(_fromUtf8("tab_2"))
+        self.verticalLayout_6 = QtGui.QVBoxLayout(self.tab_2)
+        self.verticalLayout_6.setObjectName(_fromUtf8("verticalLayout_6"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout()
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.scrollArea = QtGui.QScrollArea(self.tab_2)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName(_fromUtf8("scrollArea"))
+        self.scrollAreaWidgetContents = QtGui.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 560, 386))
+        self.scrollAreaWidgetContents.setObjectName(_fromUtf8("scrollAreaWidgetContents"))
+        self.verticalLayout_8 = QtGui.QVBoxLayout(self.scrollAreaWidgetContents)
+        self.verticalLayout_8.setObjectName(_fromUtf8("verticalLayout_8"))
+        self.remote_group = QtGui.QWidget(self.scrollAreaWidgetContents)
+        self.remote_group.setObjectName(_fromUtf8("remote_group"))
+        self.verticalLayout_11 = QtGui.QVBoxLayout(self.remote_group)
+        self.verticalLayout_11.setMargin(0)
+        self.verticalLayout_11.setObjectName(_fromUtf8("verticalLayout_11"))
+        self.remote_group_lay = QtGui.QVBoxLayout()
+        self.remote_group_lay.setObjectName(_fromUtf8("remote_group_lay"))
+        self.verticalLayout_11.addLayout(self.remote_group_lay)
+        self.verticalLayout_8.addWidget(self.remote_group)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout_2.addWidget(self.scrollArea)
+        self.label = QtGui.QLabel(self.tab_2)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout_2.addWidget(self.label)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.install_location = QtGui.QLineEdit(self.tab_2)
+        self.install_location.setObjectName(_fromUtf8("install_location"))
+        self.horizontalLayout.addWidget(self.install_location)
+        self.install_button = QtGui.QPushButton(self.tab_2)
+        self.install_button.setObjectName(_fromUtf8("install_button"))
+        self.horizontalLayout.addWidget(self.install_button)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.verticalLayout_6.addLayout(self.verticalLayout_2)
+        self.tabWidget.addTab(self.tab_2, _fromUtf8(""))
+        self.tab_3 = QtGui.QWidget()
+        self.tab_3.setObjectName(_fromUtf8("tab_3"))
+        self.verticalLayout_7 = QtGui.QVBoxLayout(self.tab_3)
+        self.verticalLayout_7.setObjectName(_fromUtf8("verticalLayout_7"))
+        self.verticalLayout_3 = QtGui.QVBoxLayout()
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.label_2 = QtGui.QLabel(self.tab_3)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.verticalLayout_3.addWidget(self.label_2)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.submit_location = QtGui.QLineEdit(self.tab_3)
+        self.submit_location.setObjectName(_fromUtf8("submit_location"))
+        self.horizontalLayout_2.addWidget(self.submit_location)
+        self.submit_button = QtGui.QPushButton(self.tab_3)
+        self.submit_button.setObjectName(_fromUtf8("submit_button"))
+        self.horizontalLayout_2.addWidget(self.submit_button)
+        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+        self.verticalLayout_7.addLayout(self.verticalLayout_3)
+        self.tabWidget.addTab(self.tab_3, _fromUtf8(""))
+        self.verticalLayout.addWidget(self.tabWidget)
+        self.accept = QtGui.QPushButton(PluginManager)
+        self.accept.setMaximumSize(QtCore.QSize(100, 16777215))
+        self.accept.setObjectName(_fromUtf8("accept"))
+        self.verticalLayout.addWidget(self.accept)
+        self.verticalLayout_4.addLayout(self.verticalLayout)
+
+        self.retranslateUi(PluginManager)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(PluginManager)
+
+    def retranslateUi(self, PluginManager):
+        PluginManager.setWindowTitle(_translate("PluginManager", "Plugin Manager", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("PluginManager", "Installed", None))
+        self.label.setText(_translate("PluginManager", "Install Plugin from URL", None))
+        self.install_button.setText(_translate("PluginManager", "Go", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("PluginManager", "Available", None))
+        self.label_2.setText(_translate("PluginManager", "Submit a URL for a plugin. See Wiki for guidelines", None))
+        self.submit_button.setText(_translate("PluginManager", "Submit", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_3), _translate("PluginManager", "Submit", None))
+        self.accept.setText(_translate("PluginManager", "OK", None))
+
