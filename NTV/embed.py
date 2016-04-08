23b61f36626dfd9e59475ee9666c0d42cca10c2e
Nate Lust <nlust@astro.princeton.edu>
2016-04-08 12:41:55 -0400

    Update embed module to look for downloaded versions of python_embed

--- NTV/embed.py
+++ NTV/embed.py
@@ -1,2 +1,18 @@
 #! /usr/bin/env python
-from .plugins.python_embed.python_embed import *
+import os
+import sys
+import NTV
+
+if sys.platform == 'darwin':
+    plug_loc = os.path.expanduser('~/Library/Application Support/NTV')
+elif sys.platform == 'win32':
+    plug_loc = os.path.join(environ['APPDATA'], 'NTV')
+else:
+    plug_loc = os.path.expanduser(os.path.join("~", "." + 'NTV'))
+
+plug_loc = os.path.join(plug_loc,'plugins')
+sys.path = [plug_loc] + sys.path
+try:
+    from python_embed.python_embed import *
+except:
+    from .plugins.python_embed.python_embed import *
